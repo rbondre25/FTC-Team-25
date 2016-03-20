cb12d2cbb40726cccf608628b422bf6967ec79a7
izzielau <izzielau@rocknrollrobots25.com>
2016-03-20 16:17:45 -0700

    Interface for an averaging (ultrasonic).

--- java/team25core/IUltrasonicAveraging.java
+++ java/team25core/IUltrasonicAveraging.java
@@ -1,7 +1,10 @@
 package team25core;
 
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
+
 /**
  * Created by Izzie on 3/19/2016.
  */
 public interface IUltrasonicAveraging {
+    double getAverage();
 }
